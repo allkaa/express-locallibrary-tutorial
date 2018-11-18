77920107df3df17d4778e429534880d5f71b5edf
AKaarna <akaarna@debian930.AccountGt.net>
2018-11-18 11:28:18 +0200

    use_pop_find

--- use_pop.js
+++ use_pop.js
@@ -27,7 +27,7 @@ const Author = require('./models/author');
 const Genre = require('./models/genre');
 const BookInstance = require('./models/bookinstance');
 
-
+/*
 function findBook(title, i, lengthArr) {
   Book.findOne({title: title}) // model Book .findOne will be executed async later so return value has no sense.
   .populate('author') //This populates the author id with actual author information!
@@ -66,6 +66,75 @@ for (let i = 0; i < bookTitle.length; i++) {
   resp = findBook(bookTitle[i], i, bookTitle.length); // returns undefined, starts async Book.findOne(...) that will be executed later.
   console.log(i, resp)
 }
+*/
+
+function bookCb(err, result) {
+  if (err) {
+    console.log('ERROR during find Book: ' + title);
+    //return 'ERROR during findOne Book: ' + title; // model Book .findOne will be executed async later so return value has no sense.
+  }
+  else {
+    if (result === null) {
+      console.log('Documents Not found => ');
+    }
+    else {
+      // result may be emptu Array(0) [] if nothing found.
+      if (result.length === 0) {
+        console.log('Documents Not found => ');
+      }
+      else {
+        for (let i = 0; i < result.length; i++) {
+          console.log('The title is %s', result[i].title); // use virtual property `name`.  Prints "The author is Rothfuss, Patrick"
+          console.log('The author is %s', result[i].author.name); // use virtual property `name`.  Prints "The author is Rothfuss, Patrick"
+          console.log('The genre is %s', result[i].genre[0].name); // use real property prints "Fantasy"
+        }
+      }
+    }
+  }
+  ///*
+  setTimeout(() => {
+    console.log(`<=== All done, disconnect from database. ==>`);
+    mongoose.connection.close();
+  }, 1000);
+  //*/
+}
+
+//let genreId = `5beea91b8cfe0caf73ba65a9`; // wrong genreId value.
+let genreId = `5beea91b8cfe0caf73ba65a3`;
+let authorId = `5beea91b8cfe0caf73ba659e`;
+Book.
+  find().
+  populate('author'). //This populates the author ID with actual author information! Otherwise author.name is undefined
+  populate('genre'). // Thie populates genre ID with actual info. Otherwise genre[i].name is undefined.
+  where('genre').equals(genreId).    // Wrong Fantasy ID  //.gt(17).lt(50).  //Additional where query
+  //where('genre').equals('5beea91b8cfe0caf73ba65a3').    // Fantasy ID  //.gt(17).lt(50).  //Additional where query
+  where('author').equals(authorId).   // Patrick Rothfuss ID  //.gt(17).lt(50).  //Additional where query
+  limit(25).
+  sort({ title: 1 }). // Asc
+  //sort({ title: -1 }). // Desc
+  //select('autor title genre'). // not needed if selecting all properties (document fields)
+  exec(bookCb); // where bookCb is the name of callback function.
+
+
+/*
+var Athlete = mongoose.model('Athlete', yourSchema);
+
+Athlete.
+  find().
+  where('sport').equals('Tennis').
+  where('age').gt(17).lt(50).  //Additional where query
+  limit(5).
+  sort({ age: -1 }).
+  select('name age').
+  exec(callback); // where callback is the name of our callback function.
+
+// All callbacks in Mongoose use the pattern callback(error, result). If an error occurs executing the query, the error parameter will contain an error document,
+// and result will be null. If the query is successful, the error parameter will be null, and the result will be populated with the results of the query.
+
+*/
+
+
+
 
 // All done, disconnect from database.
 // NB! We must close connection later manually otherwise connection will be closed BEFORE findOne() which is executed async after end of main script text.
