0c44254c614675d8c7eaef16e217c0722186bc23
AKaarna <akaarna@debian930.AccountGt.net>
2018-12-06 10:48:42 +0200

    moment

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -11,11 +12,18 @@ var BookInstanceSchema = new Schema(
   }
 );
 
-// Virtual for bookinstance's URL
+// Virtual property url for bookinstance's URL
 BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+// Virtual property due_back_formatted 
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 //Export model
