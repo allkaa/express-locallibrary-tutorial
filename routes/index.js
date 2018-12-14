9611fdfed8e133905c00c21fa4554b9d9c2e7153
AKaarna <akaarna@debian930.AccountGt.net>
2018-12-14 09:27:13 +0200

    test_pug

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express route for GET' });
-  //res.redirect('/test');
-  res.redirect('/catalog');
+  res.redirect('/test');
+  //res.redirect('/catalog');
 });
 
 module.exports = router;
