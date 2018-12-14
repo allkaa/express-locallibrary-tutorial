44271accae135362e7d47182b7ff2bf83888a5c1
AKaarna <akaarna@debian930.AccountGt.net>
2018-12-14 09:14:38 +0200

    test_pug

---
+++ controllers/test.js
@@ -0,0 +1,22 @@
+'use strict';
+/*
+var Book = require('../models/book');
+
+exports.index = function(req, res) {
+    res.send('NOT IMPLEMENTED: Site Home Page');
+};
+*/
+
+var Book = require('../models/book');
+var Author = require('../models/author');
+var Genre = require('../models/genre');
+var BookInstance = require('../models/bookinstance');
+
+var async = require('async');
+
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
+exports.test = function(req, res) {
+    res.send('NOT IMPLEMENTED: Book list');
+};
