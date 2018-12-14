77fa5b7c6133cb416ebc680c437f24165c8719c2
AKaarna <akaarna@debian930.AccountGt.net>
2018-12-14 09:47:04 +0200

    pugtest

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource profile');
+  res.render('index', { title: 'Express route for user GET' });
 //router.get('/cool', function(req, res, next) {
   //  res.send("You're so cool");
 });
