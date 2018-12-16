619eeb8fa1cc36fc5a3066259b7e40996612d8ee
AKaarna <akaarna@debian930.AccountGt.net>
2018-12-16 10:23:25 +0200

    bookinstance_delete

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -7,7 +7,7 @@ var BookInstanceSchema = new Schema(
   {
     book: { type: Schema.Types.ObjectId, ref: 'Book', required: true }, //reference to the associated book
     imprint: {type: String, required: true},
-    status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
+    status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved', 'Undefinded'], default: 'Maintenance'},
     due_back: {type: Date, default: Date.now}
   }
 );
