5faf2dbb2969d1d0ca8561e30b8c538d881ffa6b
AKaarna <akaarna@debian930.AccountGt.net>
2018-12-19 11:54:57 +0200

    genre_update

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -242,35 +242,28 @@ exports.genre_update_post =  [
       // Extract the validation errors from a request.
       const errors = validationResult(req);
   
-      // Create a genre object with escaped and trimmed data.
+      // Create a genre object with escaped and trimmed data for update.
       var genre = new Genre(
-        { name: req.body.name }
+        { 
+            name: req.body.name,
+            _id:req.params.id // NB! This is required, or a new ID will be assigned!
+        }
       );
   
       if (!errors.isEmpty()) {
         // There are errors. Render the form again with sanitized values/error messages.
-        res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array()});
+        res.render('genre_form', { title: 'Update Genre', genre: genre, errors: errors.array()});
         // e.g. errors.array() -> Array(1) [Object] -> Array()[0] [Object] -> Object {location: "body", param: "name", value: "", msg: "Genre name required"}
-      return;
+        return;
       }
       else {
         // Data from form is valid.
-        // Check if Genre with same name already exists.
-        Genre.findOne({ 'name': req.body.name })
-          .exec( function(err, found_genre) {
-             if (err) { return next(err); }
-             if (found_genre) {
-               // Genre exists, redirect to its detail page.
-               res.redirect(found_genre.url);
-             }
-             else {
-               genre.save(function (err) {
-                 if (err) { return next(err); }
-                 // Genre saved. Redirect to genre detail page.
-                 res.redirect(genre.url);
-               });
-             }
-           });
+        // Data from form is valid. Update the record.
+        Genre.findByIdAndUpdate(req.params.id, genre, {}, function (err,thegenre) {
+            if (err) { return next(err); }
+            // Successful - redirect to genre detail page.
+            res.redirect(thegenre.url);
+        });
       }
     }
 ];
