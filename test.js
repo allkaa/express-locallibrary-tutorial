33cf0ab08ba673cc6f2686f18f4011494242c687
AKaarna <akaarna@debian930.AccountGt.net>
2018-12-26 10:39:38 +0200

    deploy

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const envObj = process.env;
+for (let prop in envObj) {
+  console.log(prop + ": " + envObj[prop]);
+}
+//console.log(prop + " <== for availability test"); // NB! prop is NOT available outside of for/in loop!!!
+//dtVar = new Date();
+//console.log('====================================' + " " + dtVar.getSeconds() + "." + dtVar.getMilliseconds());
+
 /**
  * 
  * @param {*} prevValue 
